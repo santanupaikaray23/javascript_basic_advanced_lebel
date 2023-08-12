13fc045e2b6608ee3285a1cbc16997fb33fdc3ad
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2023-08-12 21:46:44 +0530

    add all files

---
+++ second.js
@@ -0,0 +1,29 @@
+var a = true
+var b = true
+
+ true+true
+2
+true+false
+1
+false+false
+0
+
+true is equal to 1
+false is equal to 0
+var a = "1a2"
+undefined
+parseInt(a)
+1
+var b = "12a"
+undefined
+parse(b)
+
+parseInt(b)
+12
+
+parseInt(c)
+
+var c = "a12"
+undefined
+parseInt(c)
+NaN
