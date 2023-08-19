a98e5fe3dbdfc46890c0fd1a9999c029b359d34b
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2023-08-19 18:01:26 +0530

    add all files

---
+++ array.js
@@ -0,0 +1,19 @@
+// Array is the collection of similar(homegenious or hetrigenious) datatype
+
+var a = [1,2,4,6,9] (Array of Number)
+var b = ['Santanu', 'Sidhanta', 'Sipun',] (Array of String)
+var c = [true,false,true,false] (Array of Boolean)
+
+var d = [2,4,5,'Santanu','Sidhanta',true,false]
+
+var city = ["London","Delhi","Newyork","Paris"]
+typeof(city)
+'object'
+pop always remove the last value of the Array
+shift remove the first value of the Array
+unshift add the begining of the Array
+push add in the end of the array
+
+var a = ['a','b','c','hii']
+var b = [1,2,3]
+a+b
