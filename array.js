fa007946b0deb291a3c055b17e29f9899f4acbd6
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2023-08-27 20:32:19 +0530

    add all changes

--- array.js
+++ array.js
@@ -17,5 +17,19 @@ push add in the end of the array
 var a = ['a','b','c','hii']
 var b = [1,2,3]
 a+b
+var city = ["London","Mumbai","Nice","Amsterdam","Dubai"]
+undefined
+city.indexOf('London')
+0
+city.indexOf('Amsterdam')
+3
+city.indexOf('Dubai')
+4
 
+var a = "hi"
+var b = ['hi']
+Array.isArray(a)
+false
+Array.isArray(b)
+true
 
---
+++ switch.js
@@ -0,0 +1,16 @@
+// Multiple condition and multiple output(ifelse)
+// One condition and one output(ternary)
+// One condition and multiple output(switch)
+
+var name = "Santanu"
+
+switch(name){
+    case 'Sidhanta':
+        console.log(`hi case 1 ${name}`)
+        break;
+        case 'Santanu':
+            console.log(`hi case 2 ${name}`)
+            break;
+            default:
+                console.log(`hi default case`)
+}
